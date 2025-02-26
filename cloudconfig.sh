c37f5e33dec0248d1f6fc245741eee9117370f2b
Anh Nguyen <anh.nguyen@cirrascale.com>
2025-02-26 13:46:35 -0800

    -eq

--- cloudconfig.sh
+++ cloudconfig.sh
@@ -17,13 +17,13 @@ dpkg -i $(basename $(curl -s -w "%{url_effective}" https://developer.download.nv
 # install kernel headers
 apt-get install -y linux-headers-$(uname -r)
 
-if [ "$CUDA" = "LATEST" ]; then
+if [ "$CUDA" -eq "LATEST" ]; then
 	apt-get install cuda-toolkit -y && apt-mark hold cuda-toolkit
 elif [ $CUDA ]; then
 	apt-get install cuda-toolkit-$CUDA -y && apt-mark hold cuda-toolkit-$CUDA
 fi
 
-if [ "$CUDA_DRIVER" = "LATEST" ]; then
+if [ "$CUDA_DRIVER" -eq "LATEST" ]; then
 	if [ $CUDA_DRIVER_FABRICMANAGER ]; then
 		apt-get install cuda-drivers-fabricmanager -y && apt-mark hold cuda-drivers-fabricmanager
 		systemctl enable nvidia-fabricmanager

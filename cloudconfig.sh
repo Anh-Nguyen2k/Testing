6d5d1789c964ed0c86f1461e24d820c00c162de5
Anh Nguyen <anh.nguyen@cirrascale.com>
2025-03-12 13:42:40 -0700

    changed to non open source drivers

--- cloudconfig.sh
+++ cloudconfig.sh
@@ -8,37 +8,37 @@ systemctl set-default multi-user.target
 
 # https://developer.nvidia.com/cuda-toolkit-archive
 arch=x86_64
-distro=ubuntu$(echo $VERSION_ID | tr -d .)
+distro=ubuntu$(echo "$VERSION_ID" | tr -d .)
 
-LATEST_CUDA_DRIVER=$(curl -s https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/ | grep -oP 'nvidia-driver-\K[0-9]+' | sort -n | tail -n 1)
+LATEST_CUDA_DRIVER=$(curl -s https://developer.download.nvidia.com/compute/cuda/repos/"$distro"/"$arch"/ | grep -oP 'nvidia-driver-\K[0-9]+' | sort -n | tail -n 1)
 NVSWITCH_FOUND=$(lspci | grep -i "Bridge: NVIDIA")
 NVL5_FOUND=$(lspci | grep -i "NVIDIA Corporation Device 2901")
 
 # https://forums.developer.nvidia.com/t/notice-cuda-linux-repository-key-rotation/212772
 # https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#network-repo-installation-for-ubuntu
-dpkg -i $(basename $(curl -s -w "%{url_effective}" https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-keyring_1.1-1_all.deb -O)) && apt-get update -y
+dpkg -i "$(basename "$(curl -s -w "%{url_effective}" https://developer.download.nvidia.com/compute/cuda/repos/"$distro"/"$arch"/cuda-keyring_1.1-1_all.deb -O)")" && apt-get update -y
 
 # install kernel headers
-apt-get install -y linux-headers-$(uname -r)
+apt-get install -y linux-headers-"$(uname -r)"
 
 # install cuda library
 if [ -n "$CUDA" ]; then
-	apt-get install -y cuda-toolkit-$CUDA -y && apt-mark hold cuda-toolkit-$CUDA
+	apt-get install -y cuda-toolkit-"$CUDA" -y && apt-mark hold cuda-toolkit-"$CUDA"
 else
 	apt-get install -y cuda-toolkit -y && apt-mark hold cuda-toolkit
 fi
 
 # install nvidia open driver
 if [ -n "$CUDA_DRIVER" ]; then
-	apt install -y nvidia-driver-$CUDA_DRIVER-open nvidia-modprobe && apt-mark hold nvidia-driver-$CUDA_DRIVER-open
+	apt install -y nvidia-driver-"$CUDA_DRIVER" nvidia-modprobe && apt-mark hold nvidia-driver-"$CUDA_DRIVER"-open
 else
-	CUDA_DRIVER=$LATEST_CUDA_DRIVER
-	apt install -y nvidia-driver-$CUDA_DRIVER-open nvidia-modprobe && apt-mark hold nvidia-driver-$CUDA_DRIVER-open
+	CUDA_DRIVER="$LATEST_CUDA_DRIVER"
+	apt install -y nvidia-driver-"$CUDA_DRIVER" nvidia-modprobe && apt-mark hold nvidia-driver-"$CUDA_DRIVER"-open
 fi
 
 # install fabricmanager for nvswitch systems
 if [ -n "$NVSWITCH_FOUND" ]; then
-		apt-get install -y nvidia-fabricmanager-$CUDA_DRIVER -y && apt-mark hold nvidia-fabricmanager-$CUDA_DRIVER
+		apt-get install -y nvidia-fabricmanager-"$CUDA_DRIVER" -y && apt-mark hold nvidia-fabricmanager-"$CUDA_DRIVER"
 		systemctl enable nvidia-fabricmanager.service --now
 fi
 
@@ -49,14 +49,14 @@ fi
 
 # install cudnn
 if [ -n "$CUDNN" ]; then
-	apt-get install -y libcudnn9-cuda-12=$CUDNN libcudnn9-dev-cuda-12=$CUDNN -y && apt-mark hold libcudnn9-cuda-12 libcudnn9-dev-cuda-12
+	apt-get install -y libcudnn9-cuda-12="$CUDNN" libcudnn9-dev-cuda-12="$CUDNN" -y && apt-mark hold libcudnn9-cuda-12 libcudnn9-dev-cuda-12
 else
 	apt-get install -y libcudnn9-cuda-12 libcudnn9-dev-cuda-12 -y && apt-mark hold libcudnn9-cuda-12 libcudnn9-dev-cuda-12
 fi
 
 # install nccl
-if [ $NCCL ]; then
-	apt-get install -y libnccl2=$NCCL libnccl-dev=$NCCL && apt-mark hold libnccl2 libnccl-dev
+if [ -n "$NCCL" ]; then
+	apt-get install -y libnccl2="$NCCL" libnccl-dev="$NCCL" && apt-mark hold libnccl2 libnccl-dev
 else
 	apt-get install -y libnccl2 libnccl-dev && apt-mark hold libnccl2 libnccl-dev
 fi

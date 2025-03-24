84d9c5185acea84687c5f1909c0f36bc1b2b5a87
Anh Nguyen <anh.nguyen@cirrascale.com>
2025-03-24 13:25:27 -0700

    cahnges

--- cloudconfig.sh
+++ cloudconfig.sh
@@ -15,45 +15,7 @@ LATEST_CUDA_DRIVER=$(curl -s https://developer.download.nvidia.com/compute/cuda/
 # Variable to store lspci output
 lspci_output=$(lspci -n)
 
-# IDs for A100, H100, B200, B100
-NVS_PCI_IDS=("1af1" "22a3" "2901" "29bc")
-NVSWITCH_FOUND=""
 
-check_nvswitch() {
-	# For loop that checks if machine matches any IDs
-	for id in "${NVS_PCI_IDS[@]}"; do
-        if echo "$lspci_output" | grep -qi "$id"; then
-            NVSWITCH_FOUND="$id"
-            return 0  # Exit function with success
-        fi
-    done
-
-	# Redundant empty variable assigment
-	NVSWITCH_FOUND=""
-    return 1  # Exit function with failure
-}
-# Run function to check for nvswitch installation
-check_nvswitch
-
-# IDs for B200, B100
-NVL5_PCI_IDS=("2901" "29bc")
-NVL5_FOUND=""
-
-check_nvl5() {
-	# For loop that checks if machine matches any IDs
-	for id in "${NVL5_PCI_IDS[@]}"; do
-        if echo "$lspci_output" | grep -qi "$id"; then
-            NVL5_FOUND="$id"
-            return 0  # Exit function with success
-        fi
-    done
-
-	# Redundant empty variable assigment
-	NVL5_FOUND=""
-    return 1  # Exit function with failure
-}
-# Run function to check for nvswitch installation
-check_nvl5
 
 # https://forums.developer.nvidia.com/t/notice-cuda-linux-repository-key-rotation/212772
 # https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#network-repo-installation-for-ubuntu
